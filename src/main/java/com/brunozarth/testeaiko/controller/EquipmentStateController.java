8461d04a15c7fdc27defb822f1c9970026c7e1c4
B.Z <brunozarth@gmail.com>
2022-06-23 17:25:22 -0300

    Adjust RequestMapping

--- src/main/java/com/brunozarth/testeaiko/controller/EquipmentStateController.java
+++ src/main/java/com/brunozarth/testeaiko/controller/EquipmentStateController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/EquipmentState")
+@RequestMapping("/api/equipmentState")
 public class EquipmentStateController {
 
     private final EquipmentStateService equipmentStateService;
